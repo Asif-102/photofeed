a15387bd70e73653114172f87ef74bf73694a5df
Asif-102 <asif1zero2@gmail.com>
2024-12-02 22:14:30 +0600

    load content using user used language

---
+++ middleware.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+const locales = ["bn", "en"];
+const defaultLocale = "en";
+
+function getLocale(request) {
+  const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
+  const headers = { "accept-language": acceptedLanguage };
+  const languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  const pathnameIsMissingLocale = locales.every(
+    (locale) => !pathname.startsWith(`/${locale}`) && pathname !== `/${locale}`
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    return NextResponse.redirect(
+      new URL(`/${locale}/${pathname}`, request.url)
+    );
+  }
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next, assets, api)
+    "/((?!api|assets|.*\\..*|_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
